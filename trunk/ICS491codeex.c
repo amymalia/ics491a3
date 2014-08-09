8d31c3d0330b75653fe27bc929c00596c4f2f62b
MilordFireblade@gmail.com <MilordFireblade@gmail.com@9f0026f4-1f90-7325-738c-14496ffa96ae>
2014-08-09 21:17:14 +0000

    Added inital SQL compatibility. Rewrote some of the program to match.

--- trunk/ICS491codeex.c
+++ trunk/ICS491codeex.c
@@ -2,186 +2,227 @@
 #include <stdlib.h>
 #include <string.h>
 
+// MySQL includes and initialization block -JS 8/9
+
+// System header include.
+#include <mysql/mysql.h>
+
+// MySQL wrapper automates error reporting.
+int mysql_wrapper(MYSQL *mysql, char *query);
+
+// MySQL variables.
+MYSQL *mysql;
+MYSQL_RES *res;
+MYSQL_ROW row;
+
+// MySQL query construction buffer.
+char querybuf[20000];
+
+// End MySQL includes and init block.
+
+// Magic numbers are bad! Pre-defining constants makes everyone's life easier.
+#define MAX_NAME_LENGTH 20
+#define MAX_PWD_LENGTH 15
+
+#define OPT_UNSET 0
+#define OPT_I_OWE_THEM 1
+#define OPT_THEY_OWE_ME 2
+#define OPT_ADD_DEBT 3
+#define OPT_CLEAR_DEBT 4
+#define OPT_EXIT 5
+
 
 void getUsername(char[]);
 void getName(char[]);
 
+// Struct for storage of user data, designed with overflow in mind.
+// Note: While these items are contiguous in code, they may not be contiguous in memory due to data structure padding.
+// See http://stackoverflow.com/questions/8377667/layout-in-memory-of-a-struct-struct-of-arrays-and-array-of-structs-in-c-c for details.
+struct userdata {
+  char username[MAX_NAME_LENGTH];
+  char password[MAX_PWD_LENGTH];
+  int access;
+};
+
 int main(int argc, char* argv[])
 {
-  char character;
-  char username[10];
-  char name[20];
-  int amount, option, access;
-  char password[15];
-  access = 0;
-  if (argc > 1)
-  {
+  struct userdata user;
+  memset(user.username, 0, sizeof(user.username));
+  memset(user.password, 0, sizeof(user.password));
+  user.access = 0;
+  
+  char name[MAX_NAME_LENGTH];
+  int amount, option;
+  if (argc > 1) {
     printf("Sorry. You have entered too many arguments.\n");
     printf("To run the debt-tracker, you must use \"a.out\" as the only argument.\n");
-	 printf("Goodbye!");
+    printf("Goodbye!");
+    return 1;
   }
-  else
-  {
-	 printf("Please enter your username: ");
-	 getUsername(username);
-	 /*if the username is valid, then move on..otherwise print an error message*/
-	 
-	 printf("Please enter your password: ");
-	 gets(password);
-	 
-	 if (strcmp(password, "correct"))
-	 {
-	   printf("Sorry, that is incorrect.\n");
-		printf("Sorry. %p is not a valid argument.\n", &password);
-		printf("%p \n", &access);
-    }
-	 else
-	 {
-	   printf("Welcome %s!\n", username);
-		access = 1;
-	 }
-	 if(access)
-	 {
-	   option = 0;
-		while (option != 5)
-		{
-	     printf("Type the number corresponding to your choice:\n");
-		  printf("1. View people who owe you money\n");
-		  printf("2. View people to whom you owe money\n");
-		  printf("3. Add a debt\n");
-		  printf("4. Remove a debt\n");
-		  printf("5. Exit the debt tracker\n");
-		  scanf("%d", &option);
-		  if (option == 1)
-		  {
-		    option = 0;
-			 while (option != 1 && option != 5)
-			 {
-		      printf("These people owe you money.\n");
-			   printf("Type the number corresponding to your choice:\n");
-			   printf("1. Return to main screen\n");
-		      printf("2. Exit the debt tracker\n");
-			   scanf("%d", &option);
-			   if (option != 1 && option != 2)
-		  	   {
-			     printf("That is not a valid option.\n");
-		      }
-			   else if (option == 2)
-			   {
-			     option = 5;
-		      }
-			 }			 
-		  }
-		  else if (option == 2)
-		  {
-		    option = 0;
-		    while (option != 1 && option != 5)
-			 {
-		      printf("You owe these people money.\n");
-			   printf("Type the number corresponding to your choice:\n");
-			   printf("1. Return to main screen\n");
-		      printf("2. Exit the debt tracker\n");
-			   scanf("%d", &option);
-			   if (option != 1 && option != 2)
-		  	   {
-			     printf("That is not a valid option.\n");
-		      }
-			   else if (option == 2)
-			   {
-			     option = 5;
-		      }
-			 }
-		  }
-		  /*need to add if-else stuffies*/
-		  else if (option == 3)
-		  {
-		    option = 0;
-			 while (option != 3 && option != 5)
-			 {
-		      printf("Type the number corresponding to your choice:\n");
-			   printf("1. You owe somebody money\n");
-			   printf("2. Somebody owes you money\n");
-			   printf("3. Return to main screen\n");
-			   printf("4. Exit the debt tracker\n");
-			 
-			   scanf("%d", &option);
-			   if (option == 1)
-			   {
-			     printf("Type the name of the person to whom you owe money: ");
-				  getName(name);
-				  printf("How much do you owe %s? ", name);
-				  /*no number checking*/
-				  scanf("%d", &amount);
-				  /*do other stuff (need to add this debt to the database..)*/
-		      }
-			   else if (option == 2)
-			   {
-			     printf("Type the name of the person who owes you money: ");
-				  getName(name);
-				  printf("How much does %s owe you? ", name);
-				  /*no number checking*/
-				  scanf("%d", &amount);
-				  /*do other stuff (need to add this debt to the database..)*/
-		      }
-			   else if (option == 4)
-			   {
-			     option = 5;
-		      }
-			   else if (option != 3)
-			   {
-			     printf("That is not a valid option.\n");
-		      }	
-		    }
-		  }
-		  else if (option == 4)
-		  {
-		    option = 0;
-			 while (option != 2 && option != 5)
-			 {
-		      printf("Type the number corresponding to your choice:\n");
-			   printf("1. Someone paid you back\n");
-			   printf("2. Return to main screen\n");
-			   printf("3. Exit the debt tracker\n");
-				
-				scanf("%d", &option);
-				
-				if (option == 1)
-				{
-				  printf("Type the name of the person who paid you back: ");
-				  getName(name);
-				  /*remove this debt from the database*/
-				}
-				else if (option == 3)
-				{
-				  option = 5;
-				}
-				else if (option != 2)
-				{
-				  printf("That is not a valid option.\n");
-				}
-		    }
-		  }
-		}
-    }
-	     
-    /*at this point, we need to connect to the database
-	 if the input compared to the password is not 0
-	   printf("Sorry, but that was an incorrect username/password combination")
-    else
-	   printf("Welcome username!")
-		access = 1
-		
-    if(access)
-	   login successful*/
+  
+  /* Initialize the MySQL instance. */
+  mysql = mysql_init(NULL);
+  if (!mysql_real_connect(mysql, "lucien.twilightparadox.com", "ics491a3", "SUPER_SECURE_ICS", "ICS491A3", 20000, NULL, 0)) {
+    fprintf(stderr, "Error while connecting to DB: %s\r\n", mysql_error(mysql));
+    return 1;
   }
+  
+  /* Obtain authentication from user. Several exploits exist here to force authentication. */
+  while (user.access == 0) {
+    // EXPLOIT - SQL INJECTION: if username is "VALID_USERNAME';--", password will not be verified.
+    printf("Please enter your username: ");
+    getUsername(user.username);
+    
+    // EXPLOIT - BUFFER OVERFLOW: Use of unbounded gets() on a bounded array allows for overflow into the ACCESS variable, granting access.
+    printf("Please enter your password: ");
+    gets(user.password);
+    
+    // Actual SQL lookup is carried out here.
+    sprintf(querybuf, "SELECT uid FROM users WHERE uid='%s', pwd='%s';", user.username, user.password);
+    if (mysql_wrapper(mysql, querybuf)) {
+      fprintf(stderr, "FATAL SQL DB ERROR. Program will now terminate.\r\n");
+      return 1;
+    }
+    res = mysql_use_result(mysql);
+    row = mysql_fetch_row(res);
+    if (!row)
+      printf("That username and password combination is invalid.");
+    else {
+      printf("Welcome %s!\n", user.username);
+      user.access = 1;
+    }
+    mysql_free_result(res);
+  }
+  
+  option = OPT_UNSET;
+  while (option != OPT_EXIT) {
+    printf("Type the number corresponding to your choice:\n");
+    printf("1. View people who owe you money\n");
+    printf("2. View people to whom you owe money\n");
+    printf("3. Add a debt\n");
+    printf("4. Remove a debt\n");
+    printf("5. Exit the debt tracker\n");
+    printf("> ");
+    scanf("%d", &option);
+    if (option == OPT_I_OWE_THEM) {
+      option = OPT_UNSET;
+      while (option != 1 && option != OPT_EXIT) {
+        printf("These people owe you money.\n");
+        printf("Type the number corresponding to your choice:\n");
+        printf("1. Return to main screen\n");
+        printf("2. Exit the debt tracker\n");
+        printf("> ");
+        scanf("%d", &option);
+        if (option != 1 && option != 2) {
+          printf("That is not a valid option.\n");
+        }
+        else if (option == 2) {
+          option = OPT_EXIT;
+        }
+      }
+    }
+    else if (option == 2) {
+      option = OPT_UNSET;
+      while (option != 1 && option != OPT_EXIT) {
+        printf("You owe these people money.\n");
+        printf("Type the number corresponding to your choice:\n");
+        printf("1. Return to main screen\n");
+        printf("2. Exit the debt tracker\n");
+        printf("> ");
+        scanf("%d", &option);
+        if (option != 1 && option != 2) {
+          printf("That is not a valid option.\n");
+        }
+        else if (option == 2) {
+          option = OPT_EXIT;
+        }
+      }
+    }
+    /*need to add if-else stuffies*/
+    else if (option == 3) {
+      option = OPT_UNSET;
+      while (option != 3 && option != OPT_EXIT) {
+        printf("Type the number corresponding to your choice:\n");
+        printf("1. You owe somebody money\n");
+        printf("2. Somebody owes you money\n");
+        printf("3. Return to main screen\n");
+        printf("4. Exit the debt tracker\n");
+        printf("> ");
+        scanf("%d", &option);
+        if (option == 1) {
+          printf("Type the name of the person to whom you owe money: ");
+          getName(name);
+          printf("How much do you owe %s? ", name);
+          /*no number checking*/
+          printf("> ");
+          scanf("%d", &amount);
+          /*do other stuff (need to add this debt to the database..)*/
+        }
+        else if (option == 2) {
+          printf("Type the name of the person who owes you money: ");
+          getName(name);
+          printf("How much does %s owe you? ", name);
+          /*no number checking*/
+          printf("> ");
+          scanf("%d", &amount);
+          /*do other stuff (need to add this debt to the database..)*/
+        }
+        else if (option == 4) {
+          option = 5;
+        }
+        else if (option != 3) {
+          printf("That is not a valid option.\n");
+        }
+      }
+    }
+    else if (option == 4)
+    {
+      option = 0;
+      while (option != 2 && option != 5)
+      {
+        printf("Type the number corresponding to your choice:\n");
+        printf("1. Someone paid you back\n");
+        printf("2. Return to main screen\n");
+        printf("3. Exit the debt tracker\n");
+        printf("> ");
+        scanf("%d", &option);
+        
+        if (option == 1)
+        {
+          printf("Type the name of the person who paid you back: ");
+          getName(name);
+          /*remove this debt from the database*/
+        }
+        else if (option == 3)
+        {
+          option = 5;
+        }
+        else if (option != 2)
+        {
+          printf("That is not a valid option.\n");
+        }
+      }
+    }
+  }
+  
+  /*at this point, we need to connect to the database
+   if the input compared to the password is not 0
+   printf("Sorry, but that was an incorrect username/password combination")
+   else
+   printf("Welcome username!")
+   access = 1
+   
+   if(access)
+   login successful*/
+  
 }
-	 
-	 
-	 
-	 
+
+
+
+
 void getUsername(char input[])
 {
-  char tempUserName[10];
+  char tempUserName[MAX_NAME_LENGTH];
   char character;
   int length;
   character = fgetc(stdin);
@@ -189,23 +230,23 @@ void getUsername(char input[])
   /*this while loop trims any white space at the beginning of the line*/
   while (character == ' ' || character == '\n')
   {
-	 character = fgetc(stdin);
+    character = fgetc(stdin);
   }
-  /*this while loop reads in characters until the end of the line is reached, 
-  or until the space reserved for the username is filled*/
-  while (character != '\n' && strlen(tempUserName) < 10)
+  /*this while loop reads in characters until the end of the line is reached,
+   or until the space reserved for the username is filled*/
+  while (character != '\n' && strlen(tempUserName) < MAX_NAME_LENGTH)
   {
     length = strlen(tempUserName);
     tempUserName[length] = character;
     tempUserName[length + 1] = '\0';
-	 character = fgetc(stdin);
+    character = fgetc(stdin);
   }
   strcpy(input, tempUserName);
 }
 
 void getName(char input[])
 {
-  char tempName[20];
+  char tempName[MAX_NAME_LENGTH];
   char character;
   int length;
   character = fgetc(stdin);
@@ -213,17 +254,24 @@ void getName(char input[])
   /*this while loop trims any white space at the beginning of the line*/
   while (character == ' ' || character == '\n')
   {
-	 character = fgetc(stdin);
+    character = fgetc(stdin);
   }
-  /*this while loop reads in characters until the end of the line is reached, 
-  or until the space reserved for the username is filled*/
-  while (character != '\n' && strlen(tempName) < 20)
+  /*this while loop reads in characters until the end of the line is reached,
+   or until the space reserved for the username is filled*/
+  while (character != '\n' && strlen(tempName) < MAX_NAME_LENGTH)
   {
     tempName[length] = character;
     tempName[length + 1] = '\0';
-	 length += 1;
-	 character = fgetc(stdin);
+    length += 1;
+    character = fgetc(stdin);
   }
   strcpy(input, tempName);
 }
 
+// MySQL wrapper to automate some of the error reporting. -JS 8/9
+int mysql_wrapper(MYSQL *mysql, char *query)
+{
+  if (mysql_errno(mysql))
+    fprintf(stderr, "MYSQLERROR: %s (generated from query %s)", mysql_error(mysql), query);
+  return mysql_query(mysql, query);
+}
